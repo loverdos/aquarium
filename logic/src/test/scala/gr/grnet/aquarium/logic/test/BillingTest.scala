21f2b3439c0de5cb5958a86e4c3d848640e9b467
Georgios Gousios <gousiosg@gmail.com>
2011-10-07 10:27:13 +0200

    Simple test for the accounting system

--- logic/src/test/scala/gr/grnet/aquarium/logic/test/BillingTest.scala
+++ logic/src/test/scala/gr/grnet/aquarium/logic/test/BillingTest.scala
@@ -2,7 +2,11 @@ package gr.grnet.aquarium.logic.test
 
 import org.junit._
 import gr.grnet.aquarium.logic.Bills
-import gr.grnet.aquarium.model.{Entity, DB}
+import gr.grnet.aquarium.model.{User, DB}
+import gr.grnet.aquarium.logic.accounting.agreements.DefaultAgreement
+import gr.grnet.aquarium.logic.accounting.policies.DefaultRatePolicy
+import gr.grnet.aquarium.logic.accounting.{AccountingEvent, AccountingEntryType, AccountingEventType}
+import java.util.Date
 
 class BillingTest
   extends FixtureLoader with Bills {
@@ -16,11 +20,21 @@ class BillingTest
 
   @Test
   def testAccountingRules() = {
-    
+    DefaultAgreement.addPolicy(AccountingEventType.VMTime, new DefaultRatePolicy(AccountingEntryType.VMTIME_CHARGE), new Date(2))
+    DefaultAgreement.addPolicy(AccountingEventType.DiskSpace, new DefaultRatePolicy(AccountingEntryType.STORAGE_CHARGE), new Date(2))
+    DefaultAgreement.addPolicy(AccountingEventType.NetDataDown, new DefaultRatePolicy(AccountingEntryType.NET_CHARGE), new Date(2))
+    DefaultAgreement.addPolicy(AccountingEventType.NetDataUp, new DefaultRatePolicy(AccountingEntryType.NET_CHARGE), new Date(2))
+
+    val u = new User
+    u.agreement = DefaultAgreement.id
+    u.credits = 100
+
+    val evt = new AccountingEvent(AccountingEventType.VMTime, new Date(4), 1, 15, List())
+    evt.process()
   }
 
   @After
   def after() = {
-    DB.getTransaction.rollback()
+    DB.getTransaction.rollback
   }
 }
