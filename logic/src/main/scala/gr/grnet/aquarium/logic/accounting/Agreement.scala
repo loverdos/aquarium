cda175fbd5e4c42e070c8afd3dce3965e663b8c1
Georgios Gousios <gousiosg@gmail.com>
2011-10-07 08:04:54 +0200

    Make it compile

--- logic/src/main/scala/gr/grnet/aquarium/logic/accounting/Agreement.scala
+++ logic/src/main/scala/gr/grnet/aquarium/logic/accounting/Agreement.scala
@@ -2,18 +2,21 @@ package gr.grnet.aquarium.logic.accounting
 
 import java.util.Date
 import collection.mutable.{HashMap, Map}
+import gr.grnet.aquarium.logic.accounting.AccountingEventType
 
 abstract class Agreement {
+
+  var pricelist : Map[AccountingEventType.Value, Float] = _
 
   var policies = new HashMap[AccountingEventType.Value, HashMap[Date,Policy]]
 
   def addPolicy(et: AccountingEventType.Value, p: Policy, d: Date) = {
-    policies + Map(et -> Map(d -> p))
+    policies ++ Map(et -> Map(d -> p))
   }
 
   def policy(et: AccountingEventType.Value, d: Date) : Option[Policy] = {
     val ruleset = policies.getOrElse(et, new HashMap[Date, Policy])
-    val key = ruleset.keys.toList.sorted.find(k => k.compareTo(d) >= 0).orElse(new Date())
+    val key = ruleset.keys.toList.sorted.find(k => k.compareTo(d) >= 0).getOrElse(new Date())
     ruleset.get(key)
   }
 }
