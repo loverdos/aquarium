4c759c0d2bbbce7793796200a468e3a354db86e1
Georgios Gousios <gousiosg@gmail.com>
2011-10-21 21:52:08 +0300

    Start processing policy inheritance

--- logic/src/main/scala/gr/grnet/aquarium/logic/accounting/dsl/DSL.scala
+++ logic/src/main/scala/gr/grnet/aquarium/logic/accounting/dsl/DSL.scala
@@ -2,9 +2,9 @@ package gr.grnet.aquarium.logic.accounting.dsl
 
 import org.yaml.snakeyaml.Yaml
 import java.io.InputStream
-import java.util.{Date}
 import scala.collection.JavaConversions._
 import org.slf4j.{LoggerFactory}
+import java.util.Date
 
 class DSL
 
@@ -12,7 +12,7 @@ object DSL {
 
   val logger = LoggerFactory.getLogger(classOf[DSL])
 
-  def parse(input: InputStream) = {
+  def parse(input: InputStream) : DSLCreditPolicy = {
     val yaml = new Yaml()
     val policy = yaml.load(input).asInstanceOf[java.util.Map[String,_]].get("creditpolicy")
 
@@ -25,10 +25,68 @@ object DSL {
 
     val policies = asScalaBuffer(policy.asInstanceOf[java.util.Map[String,_]]
                                         .get("policies")
-                                        .asInstanceOf[java.util.List[String]]).toList
+                                        .asInstanceOf[java.util.List[_]])
 
-    val dslpolicy = DSLCreditPolicy(List(), List(), List(), List())
+    policies.map(
+      p => mapAsScalaMap(p.asInstanceOf[java.util.Map[String,_]])
+    )
+
+
+    DSLCreditPolicy(List(), List(), List(), List())
   }
+
+  def parsePolicies(policies: List[Map[String,_]],
+                    resources: List[DSLResource],
+                    results: List[DSLPolicy]): List[DSLPolicy] = {
+
+    val supr = policies.head.getOrElse("extends", None)
+
+    
+
+    val result = DSLPolicy("", "", Map(), DSLTimeFrame("", new Date(0), new Date(1), List()))
+    val tmpresults = results ++ List(result)
+    List(result) ++ parsePolicies(policies.tail, resources, tmpresults)
+  }
+
+  /*def getPolicy(policy: DSLPolicy, policies: List[DSLPolicy]) : DSLPolicy = {
+    policy.overrides match {
+      case x: String => getPolicy(policy, policies)
+      case None => policy
+    }
+  }*/
+
+  def constructPolicy(policy: Map[String, String], resources: List[DSLResource]) : DSLPolicy = {
+    val name = policy.getOrElse("name", None).asInstanceOf[String]
+    val overr = policy.getOrElse("overrides", None).asInstanceOf[String]
+    val algos = resources.map {
+      r =>
+        val algo = policy.get(r.name) match {
+          case Some(x) => x
+          case None => ""
+        }
+        Map(r -> algo)
+    }
+
+    DSLPolicy(name, overr,
+              mergeMaps(algos)((v1: String, v2: String) => v1), null)
+  }
+
+  def mergePolicy(policy: DSLPolicy, onto: DSLPolicy) : DSLPolicy = {
+    DSLPolicy(onto.name, onto.overrides,
+              mergeMaps(policy.algorithms, onto.algorithms), null)
+  }
+
+  def mergeMaps[A, B](a: Map[A, B], b: Map[A, B]): Map[A, B] = {
+    a ++ b.map{ case (k,v) => k -> (a.getOrElse(k,v)) }
+  }
+
+  def mergeMaps[A, B](ms: List[Map[A, B]])(f: (B, B) => B): Map[A, B] =
+    (Map[A, B]() /: (for (m <- ms; kv <- m) yield kv)) {
+      (a, kv) =>
+        a + (if (a.contains(kv._1))
+              kv._1 -> f(a(kv._1), kv._2)
+            else kv)
+    }
 }
 
 case class DSLCreditPolicy (
--- logic/src/test/scala/gr/grnet/aquarium/logic/test/DSLTest.scala
+++ logic/src/test/scala/gr/grnet/aquarium/logic/test/DSLTest.scala
@@ -1,12 +1,34 @@
 package gr.grnet.aquarium.logic.test
 
 import org.junit.Test
-import gr.grnet.aquarium.logic.accounting.dsl.DSL
+import org.junit.Assert._
+import gr.grnet.aquarium.logic.accounting.dsl._
 
 class DSLTest {
 
   @Test
   def testDSLLoad = {
-    DSL.parse(getClass.getClassLoader.getResourceAsStream("policy.yaml"))
+    var policy = DSL.parse(
+      getClass.getClassLoader.getResourceAsStream("policy.yaml")
+    )
+    assertNotNull(policy)
+  }
+
+  @Test
+  def testMergePolicies = {
+    val vm = DSLResource("vmtime")
+    val bup = DSLResource("bup")
+    val bdown = DSLResource("bdown")
+
+    val a = DSLPolicy("1", "2", Map(vm -> "abc", bup -> "def"), null)
+    val b = DSLPolicy("2", "", Map(vm -> "xyz", bdown -> "foo"), null)
+
+    val result = DSL.mergePolicy(a, b)
+
+    assertEquals(result.name, "2")
+    assertEquals(result.algorithms.size, 3)
+    assertEquals(result.algorithms.get(vm), Some("abc"))
+    assertEquals(result.algorithms.get(bup), Some("def"))
+    assertEquals(result.algorithms.get(bdown), Some("foo"))
   }
 }
