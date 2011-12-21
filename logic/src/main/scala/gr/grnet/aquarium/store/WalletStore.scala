13c6a9ff00b5366182b109e49c97de0e15293519
Georgios Gousios <gousiosg@gmail.com>
2011-12-21 10:56:48 +0200

    Interface to the users' Wallet

---
+++ logic/src/main/scala/gr/grnet/aquarium/store/WalletStore.scala
@@ -0,0 +1,21 @@
+package gr.grnet.aquarium.store
+
+import gr.grnet.aquarium.logic.events.{WalletEntry}
+import java.util.Date
+import com.ckkloverdos.maybe.Maybe
+
+/**
+ * A store for Wallets
+ *
+ * @author Georgios Gousios <gousiosg@gmail.com>
+ */
+trait WalletStore {
+
+  def store(entry: WalletEntry): Maybe[RecordID]
+
+  def findEntryById(id: String): Option[WalletEntry]
+
+  def findAllUserEntries(userId: String): List[WalletEntry]
+
+  def findUserEntriesFromTo(userId: String, from: Date, to: Date): List[WalletEntry]
+}
