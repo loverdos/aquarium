c930eea357560a6d9534260eabcd807b37127b6c
Christos KK Loverdos <loverdos@gmail.com>
2011-12-29 12:18:20 +0200

    Use an immutable collection for source event IDs

--- src/main/scala/gr/grnet/aquarium/logic/events/WalletEntry.scala
+++ src/main/scala/gr/grnet/aquarium/logic/events/WalletEntry.scala
@@ -8,13 +8,14 @@ import net.liftweb.json._
  * @author Georgios Gousios <gousiosg@gmail.com>
  * @author Christos KK Loverdos <loverdos@gmail.com>
  */
-case class WalletEntry(override val id: String,
-                       override val occurredMillis: Long,
-                       sourceEventIDs: Array[String], // The events that triggered this WalletEntry
-                       value: Float,
-                       reason: String,
-                       userId: String,
-                       finalized: Boolean)
+case class WalletEntry(
+    override val id: String,
+    override val occurredMillis: Long,
+    sourceEventIDs: List[String], // The events that triggered this WalletEntry
+    value: Float,
+    reason: String,
+    userId: String,
+    finalized: Boolean)
   extends AquariumEvent(id, occurredMillis) {
 
   assert(occurredMillis > 0)
@@ -31,5 +32,5 @@ object WalletEntry {
     Extraction.extract[ResourceEvent](jsonAST)
   }
 
-  def zero = WalletEntry("", 1L, Array(),1,"","foo", false)
+  def zero = WalletEntry("", 1L, Nil,1,"","foo", false)
 }
