c5ca558f3461e6f3e407d68c960f6c69a3f654ce
Christos KK Loverdos <loverdos@gmail.com>
2012-02-08 19:40:42 +0200

    Augment date calculator

--- src/main/scala/gr/grnet/aquarium/util/date/DateCalculator.scala
+++ src/main/scala/gr/grnet/aquarium/util/date/DateCalculator.scala
@@ -1,3 +1,38 @@
+/*
+ * Copyright 2011-2012 GRNET S.A. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ *   1. Redistributions of source code must retain the above
+ *      copyright notice, this list of conditions and the following
+ *      disclaimer.
+ *
+ *   2. Redistributions in binary form must reproduce the above
+ *      copyright notice, this list of conditions and the following
+ *      disclaimer in the documentation and/or other materials
+ *      provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY GRNET S.A. ``AS IS'' AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL GRNET S.A OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and
+ * documentation are those of the authors and should not be
+ * interpreted as representing official policies, either expressed
+ * or implied, of GRNET S.A.
+ */
+
 package gr.grnet.aquarium.util.date
 
 import org.joda.time.{MutableDateTime, DateMidnight}
@@ -23,9 +58,57 @@ class DateCalculator private(private[this] var dateTime: MutableDateTime) extend
     this(year, monthOfYear, 1)
 
 
-  override def clone(): DateCalculator = new DateCalculator(this.dateTime)
+  override def clone(): DateCalculator = new DateCalculator(this.dateTime.clone().asInstanceOf[MutableDateTime])
 
   def copy: DateCalculator = clone()
+
+  def <=(other: DateCalculator): Boolean = {
+    getMillis <= other.getMillis
+  }
+
+  def <=(other: Date): Boolean = {
+    getMillis <= other.getTime
+  }
+
+  def <=(other: Long): Boolean = {
+    getMillis <= other
+  }
+
+  def <(other: DateCalculator): Boolean = {
+    getMillis < other.getMillis
+  }
+
+  def <(other: Date): Boolean = {
+    getMillis < other.getTime
+  }
+
+  def <(other: Long): Boolean = {
+    getMillis < other
+  }
+
+  def >=(other: DateCalculator): Boolean = {
+    getMillis >= other.getMillis
+  }
+
+  def >=(other: Date): Boolean = {
+    getMillis >= other.getTime
+  }
+
+  def >=(other: Long): Boolean = {
+    getMillis >= other
+  }
+
+  def >(other: DateCalculator): Boolean = {
+    getMillis >= other.getMillis
+  }
+
+  def >(other: Date): Boolean = {
+    getMillis > other.getTime
+  }
+
+  def >(other: Long): Boolean = {
+    getMillis > other
+}
 
   def plusMonths(n: Int): this.type = {
     dateTime.addMonths(n)
@@ -147,7 +230,7 @@ class DateCalculator private(private[this] var dateTime: MutableDateTime) extend
   }
 
   /**
-   * At the first millisecond of this month. This month is the month indicated by the
+   * Go to the first millisecond of this month. This month is the month indicated by the
    * state of the [[gr.grnet.aquarium.util.date.DateCalculator]] and not the real-life month.
    */
   def goStartOfThisMonth: this.type = {
@@ -159,14 +242,21 @@ class DateCalculator private(private[this] var dateTime: MutableDateTime) extend
   }
 
   /**
-   * At the first millisecond of the next month.
+   * Go to the first millisecond of the next month.
    */
   def goStartOfNextMonth: this.type = {
     goNextMonth.goStartOfThisMonth
   }
 
   /**
-   * At the last millisecond of the month
+   * Go to the first millisecond of the previous month.
+   */
+  def goStartOfPreviousMongth: this.type = {
+    goPreviousMonth.goStartOfThisMonth
+  }
+
+  /**
+   * Go to the last millisecond of the month
    */
   def goEndOfThisMonth: this.type = {
     goStartOfNextMonth.goPreviousMilli
