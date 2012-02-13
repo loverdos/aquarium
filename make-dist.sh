d74cbebece5e0a2cde303c1f3f2f1dbcb69bf7ab
Georgios Gousios <gousiosg@gmail.com>
2012-02-13 15:56:57 +0200

    Factor out clean and fail operations in methods

--- make-dist.sh
+++ make-dist.sh
@@ -3,6 +3,24 @@
 # For debugging
 clear
 # set -x
+LOG=build.log
+
+fail() {
+    echo "failed while $1"
+    echo "last log lines are"
+    tail -n 15 $LOG
+    echo "see file $LOG for details"
+    cleanup
+    exit 1
+}
+
+cleanup() {
+    git checkout master 2>&1 >>$LOG
+    if [ -f stashed ]; then
+        git stash pop 
+        rm stashed
+    fi
+}
 
 # Check if there are pending changes
 status=`git status --porcelain|grep -v "??"`
@@ -12,7 +30,7 @@ if [ ! -z "$status" ]; then
     echo $status
     echo
     echo "Stashing them"
-    git stash save
+    git stash save 2>&1 >>$LOG
     touch stashed
 fi
 
@@ -29,25 +47,21 @@ mkdir -p $DIR/conf
 mkdir -p $DIR/logs
 
 echo "Checking out $tag"
-git checkout $tag >/dev/null || exit 1
+git checkout $tag 2>&1 1>>$LOG || fail "producing checkout"
 
 echo "Building $tag"
-mvn clean install 1>/dev/null || exit 1
+mvn clean install -DskipTests=true >>build.log || fail "building project"
 
-echo "Gathering dependencies"
-mvn dependency:copy-dependencies > /dev/null || exit 1
+echo "Collecting dependencies"
+mvn dependency:copy-dependencies >> build.log ||  fail "collecting dependencies"
 cp target/dependency/*.jar $DIR/lib
 
-tar zcvf aquarium-$tag.tar.gz $DIR > /dev/null || exit 1
+echo "Creating archive"
+tar zcvf aquarium-$tag.tar.gz $DIR >> build.log 2>&1 || fail "creating archive"
 
 echo "Cleaning up"
 rm -Rf $DIR
-
-# Revert working dir
-git checkout master
-if [ -f stashed ]; then
-    git stash pop >/dev/null
-    rm stashed
-fi
+cleanup
+rm $LOG
 
 # vim: set sta sts=4 shiftwidth=4 sw=4 et ai :
