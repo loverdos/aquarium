4613d640f46b399937d10801019772221699ce46
Christos KK Loverdos <loverdos@gmail.com>
2012-02-13 17:40:42 +0200

    Introduce cost policy vars
    These are the variables needed by each cost policy in order to do the
    credit charge calculations. Normally, they are used by the algorithms
    that do the actual calculations.

---
+++ src/main/scala/gr/grnet/aquarium/logic/accounting/dsl/DSLCostPolicyVar.scala
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2011 GRNET S.A. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ *   1. Redistributions of source code must retain the above
+ *      copyright notice, this list of conditions and the following
+ *      disclaimer.
+ *
+ *   2. Redistributions in binary form must reproduce the above
+ *      copyright notice, this list of conditions and the following
+ *      disclaimer in the documentation and/or other materials
+ *      provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY GRNET S.A. ``AS IS'' AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL GRNET S.A OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and
+ * documentation are those of the authors and should not be
+ * interpreted as representing official policies, either expressed
+ * or implied, of GRNET S.A.
+ */
+
+package gr.grnet.aquarium.logic.accounting.dsl
+
+/**
+ * The type of variable needed in order to compute costs, that is credit charges.
+ *
+ * A [[gr.grnet.aquarium.logic.accounting.dsl.DSLCostPolicy]] declares which variables
+ * it needs in order to compute credit charges. These and only these variables are
+ * passed to the repsective [[gr.grnet.aquarium.logic.accounting.dsl.DSLAlgorithm]] (and
+ * are also used to type-check the algorithm).
+ *
+ * @author Christos KK Loverdos <loverdos@gmail.com>
+ */
+sealed abstract class DSLCostPolicyVar(val name: String)
+
+/**
+ * The type of [[gr.grnet.aquarium.logic.accounting.dsl.DSLCostPolicyVar]] that holds the total credits.
+ *
+ * @author Christos KK Loverdos <loverdos@gmail.com>
+ */
+case object DSLTotalCreditsVar extends DSLCostPolicyVar("totalCredits")
+
+/**
+ * The type of [[gr.grnet.aquarium.logic.accounting.dsl.DSLCostPolicyVar]] that holds the total (accumulating) amount.
+ * For example, in the case of `diskspace`, this is the total diskspace used by a user.
+ *
+ * @author Christos KK Loverdos <loverdos@gmail.com>
+ */
+case object DSLTotalAmountVar extends DSLCostPolicyVar("totalAmount")
+
+/**
+ * The type of [[gr.grnet.aquarium.logic.accounting.dsl.DSLCostPolicyVar]] that holds the time delta between two
+ * consecutive resource events of the same type (same `resource` and `instanceId`).
+ *
+ * @author Christos KK Loverdos <loverdos@gmail.com>
+ */
+case object DSLTimeDeltaVar extends DSLCostPolicyVar("timeDelta")
+
+/**
+ * The type of [[gr.grnet.aquarium.logic.accounting.dsl.DSLCostPolicyVar]] that holds the `value` of the previous
+ * [[gr.grnet.aquarium.logic.events.ResourceEvent]].
+ *
+ * @author Christos KK Loverdos <loverdos@gmail.com>
+ */
+case object DSLPreviousVar extends DSLCostPolicyVar("previousValue")
+
+/**
+ * The type of [[gr.grnet.aquarium.logic.accounting.dsl.DSLCostPolicyVar]] that holds the `value` of the next
+ * [[gr.grnet.aquarium.logic.events.ResourceEvent]].
+ *
+ * @author Christos KK Loverdos <loverdos@gmail.com>
+ */
+case object DSLCurrentVar extends DSLCostPolicyVar("currentValue")
